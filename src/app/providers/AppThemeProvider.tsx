9298ac9486729b126988848faeeca16be5eba80e
Tnalxmsk <alstnwkd990@naver.com>
2025-05-25 05:38:58 +0900

    :sparkles: feat: Theme Provider 구현

--- src/app/providers/AppThemeProvider.tsx
+++ src/app/providers/AppThemeProvider.tsx
@@ -1,6 +1,6 @@
 import { ThemeProvider } from "@emotion/react";
-import type { ReactNode } from "react";
 import theme from "@app/styles/theme.ts";
+import type { ReactNode } from "react";
 
 export const AppThemeProvider = ({ children }: { children: ReactNode }) => {
   return <ThemeProvider theme={theme}>{children}</ThemeProvider>;
