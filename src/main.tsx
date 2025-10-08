f527a1d0aa621585ba5d1a66cef5e512561e98e1
Tnalxmsk <alstnwkd990@naver.com>
2025-10-08 22:46:43 +0900

    :sparkles: feat: Set Browser MSW

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,10 @@
 import { createRoot } from 'react-dom/client';
 import App from "@app/App";
 
+if (import.meta.env.DEV) {
+  const { browser } = await import('@shared/mock/browser')
+  await browser.start({ onUnhandledRequest: 'warn' });
+}
 
 createRoot(document.getElementById('root')!).render(
   <App />,
