5f443b8e6955f342ef5457a3f77ad59ec8c9dd21
Tnalxmsk <alstnwkd990@naver.com>
2025-10-08 23:29:39 +0900

    :bug: fix: prod error

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,9 @@
 import { createRoot } from 'react-dom/client';
 import App from "@app/App";
 
-if (import.meta.env.DEV) {
-  const { browser } = await import('@shared/mock/browser')
+// if (import.meta.env.DEV) {
+if (import.meta.env.VITE_ENABLE_MSW === 'true') {
+  const { browser } = await import('@shared/mock/browser');
   await browser.start({ onUnhandledRequest: 'warn' });
 }
 
