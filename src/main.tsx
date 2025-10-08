ed4efcdd4efd8f92db3bfc8354f09de0b0cc4acc
Tnalxmsk <alstnwkd990@naver.com>
2025-10-08 23:42:54 +0900

    :bug: fix: fix build error

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,16 @@
 import { createRoot } from 'react-dom/client';
 import App from "@app/App";
 
-// if (import.meta.env.DEV) {
-if (import.meta.env.VITE_ENABLE_MSW === 'true') {
-  const { browser } = await import('@shared/mock/browser');
-  await browser.start({ onUnhandledRequest: 'warn' });
+async function bootstrap() {
+  if (import.meta.env.VITE_ENABLE_MSW === 'true') {
+    const { browser } = await import('@shared/mock/browser');
+    await browser.start({
+      onUnhandledRequest: 'warn',
+    });
+  }
+
+  const container = document.getElementById('root')!;
+  createRoot(container).render(<App />);
 }
 
-createRoot(document.getElementById('root')!).render(
-  <App />,
-);
+bootstrap();
