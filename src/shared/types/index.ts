ae4b6afe7eab6f475e95d7da556ad6821b3ba864
Tnalxmsk <alstnwkd990@naver.com>
2025-10-08 22:49:40 +0900

    :recycle: refactor: index.ts for mock data, type

---
+++ src/shared/types/index.ts
@@ -0,0 +1,4 @@
+export { type AboutMeResponse, questions } from './aboutMe.ts';
+export { type SkillCategory, skillCategories } from './skills.ts';
+export { type Experience, type ExperienceType, experiences } from './experience.ts';
+export { type Project, projects } from './projects.ts'
